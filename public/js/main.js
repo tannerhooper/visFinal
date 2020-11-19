1af2ed7bca11e856ba2320da566654a9830d9ffb
connernance1 <conner.nance1@gmail.com>
2020-11-18 21:59:03 -0700

    Working map. Need to add data and color scale to map

--- public/js/main.js
+++ public/js/main.js
@@ -2,11 +2,10 @@
 // TODO: Create instances of all charts
 let spendChart = new SpendChart();
 // Load the data corresponding to all the years.
-d3.csv("data/yearwiseDropouts.csv").then(yearlyDropouts => {
-    let yearChart = new YearChart(spendChart, yearlyDropouts);// TODO: pass chart instances 
-    yearChart.update();
-  
-    let s = d3.select('#y2018');
-    yearChart.selectYear(s, s.data()[0]);
-  });
-  
+d3.csv("data/yearwiseDropouts.csv", function (yearlyDropouts) {
+  let yearChart = new YearChart(spendChart, yearlyDropouts);// TODO: pass chart instances 
+  yearChart.update();
+
+  let s = d3.select('#y2018');
+  yearChart.selectYear(s, s.data()[0]);
+});
