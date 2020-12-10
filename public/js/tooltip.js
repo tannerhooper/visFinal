ded5e019945d8dba401c2c28b31e6affe9c99c8d
Laryssa Revelli <lreesaguymon@gmail.com>
2020-12-09 21:28:14 -0700

    Working tooltip

--- public/js/tooltip.js
+++ public/js/tooltip.js
@@ -25,36 +25,41 @@ class Tooltip {
       "TN": "Tennessee","TX": "Texas","UT": "Utah","VT": "Vermont","VA": "Virginia","WA": "Washington",
       "WV": "West Virginia","WI": "Wisconsin","WY": "Wyoming"
     }
-    this.cols = [ "C150_4_2MOR", "C150_4_AIAN", "C150_4_ASIAN", "C150_4_BLACK", "C150_4_HISP", "C150_4_NHPI", "C150_4_NRA", "C150_4_UNKN", "C150_4_WHITE"];
   };
   
   /**
    * Gets the HTML content for a tool tip.
    */
   tooltip_html(data, d) {
-    var cols = this.cols;
+    var cols = [ "C150_4_2MOR", "C150_4_AIAN", "C150_4_ASIAN", "C150_4_BLACK", "C150_4_HISP", "C150_4_NHPI", "C150_4_NRA", "C150_4_UNKN", "C150_4_WHITE"];
+    var colMapping = {
+      "C150_4_2MOR": "2 or More", "C150_4_AIAN": "Native American", "C150_4_ASIAN": "Asian",
+      "C150_4_BLACK": "Black", "C150_4_HISP": "Hispanic", "C150_4_NHPI": "Pacific Islander",
+      "C150_4_NRA": "Nonresident Alien", "C150_4_UNKN": "Unknown", "C150_4_WHITE": "White"
+    };
     var perct = 0;
     let text = "<h2>" + `${this.stateMapping[d]}` + "</h2>";
     cols.forEach(function(race) {
       var dta = data.filter(dt => {
         return dt.STABBR === d && dt[race] !== 'NULL'});
-        // console.log(dta.race)
-        // dta[race].reduce((a, b) => (a + b)) / dta.length;
-        perct = parseFloat(data.filter(dt => {
-          return dt.STABBR === d && dt[race] !== 'NULL'}));
+        perct = dta.map(dt => {
+            return dt[race];
+          }).reduce((p, c) => +p + +c, 0) / dta.length;
+          perct *= 100;
+          perct = perct.toFixed(0);
           text += `<table>  
           <tr>
-            <td>`+`${race}`+ `</td> 
+            <td>`+`${colMapping[race]}`+ `</td> 
             <td>
-            <div style=\"width:{{ 1.3*`+ `${perct}` + `}}px; 
+            <div style=\"width: 1.3*`+ `${perct/100}` + `px; 
                   height:15px; background-color:#BFA817; color:white; 
                   padding:4px 4px 0px 4px; vertical-align:bottom; 
                   font-weight:bold; display:inline-block;\">
             </div> 
-            <div style=\"width:{{ 130-(1.3*`+ `${perct}` + `) }}px;
+            <div style=\"width: 130-(1.3*`+ `${perct/100}` + `) px;
                   height:15px; background-color:#F2F2F2; color:#a69214; 
                   vertical-align:bottom; padding:4px 4px 0px 4px;
-                  font-weight:bold; display:inline-block;\">{{ `+ `${perct}` + ` }}% 
+                  font-weight:bold; display:inline-block;\"> `+ `${perct}` + `% 
             </div>
             </td>
           </tr>  
