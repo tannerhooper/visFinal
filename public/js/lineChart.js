a219283edac9b9cfce792a0b2d055b45c041d5aa
Tanner Hooper <tanner@Tanners-MacBook-Pro.local>
2020-12-09 19:37:02 -0700

    adds in tools

--- public/js/lineChart.js
+++ public/js/lineChart.js
@@ -8,20 +8,8 @@ class LineChart {
         this.chart = d3.select(`#line-chart`).classed("sideBar",true);
         this.allYears = allYears;
         this.years = years;
-
-        this.stateMapping = {
-            "AL": "Alabama","AK": "Alaska","AZ": "Arizona","AR": "Arkansas","CA": "California",
-            "CO": "Colorado","CT": "Connecticut","DE": "Delaware","FL": "Florida","GA": "Georgia",
-            "HI": "Hawaii","ID": "Idaho","IL": "Illinois","IN": "Indiana","IA": "Iowa","KS": "Kansas",
-            "KY": "Kentucky","LA": "Louisiana","ME": "Maine","MD": "Maryland","MA": "Massachusetts",
-            "MI": "Michigan","MN": "Minnesota","MS": "Mississippi","MO": "Missouri","MT": "Montana",
-            "NE": "Nebraska","NV": "Nevada","NH": "New Hampshire","NJ": "New Jersey","NM": "New Mexico",
-            "NY": "New York","NC": "North Carolina","ND": "North Dakota","OH": "Ohio","OK": "Oklahoma",
-            "OR": "Oregon","PA": "Pennsylvania","RI": "Rhode Island","SC": "South Carolina","SD": "South Dakota",
-            "TN": "Tennessee","TX": "Texas","UT": "Utah","VT": "Vermont","VA": "Virginia","WA": "Washington",
-            "WV": "West Virginia","WI": "Wisconsin","WY": "Wyoming"
-          }
-
+        
+        // variables for filters
         this.selState = '';
         this.bounds = [];
         this.demoFilter = '';
@@ -70,7 +58,7 @@ class LineChart {
         if (curYr !== null) this.curYr = curYr;
 
         // Sets line chart title
-        d3.select('#stAvg').text(`State Average: ${this.stateMapping[this.selState]}`)
+        d3.select('#stAvg').text(`State Average: ${Tools.stateMapping[this.selState]}`)
         
         // Computes avg per state
         for (let t in this.allYears){
--- public/js/tooltip.js
+++ public/js/tooltip.js
@@ -13,18 +13,6 @@ class Tooltip {
       .attr('id', 'tooltip')
       .classed('tooltipDiv', true)
     ;
-    this.stateMapping = {
-      "AL": "Alabama","AK": "Alaska","AZ": "Arizona","AR": "Arkansas","CA": "California",
-      "CO": "Colorado","CT": "Connecticut","DE": "Delaware","FL": "Florida","GA": "Georgia",
-      "HI": "Hawaii","ID": "Idaho","IL": "Illinois","IN": "Indiana","IA": "Iowa","KS": "Kansas",
-      "KY": "Kentucky","LA": "Louisiana","ME": "Maine","MD": "Maryland","MA": "Massachusetts",
-      "MI": "Michigan","MN": "Minnesota","MS": "Mississippi","MO": "Missouri","MT": "Montana",
-      "NE": "Nebraska","NV": "Nevada","NH": "New Hampshire","NJ": "New Jersey","NM": "New Mexico",
-      "NY": "New York","NC": "North Carolina","ND": "North Dakota","OH": "Ohio","OK": "Oklahoma",
-      "OR": "Oregon","PA": "Pennsylvania","RI": "Rhode Island","SC": "South Carolina","SD": "South Dakota",
-      "TN": "Tennessee","TX": "Texas","UT": "Utah","VT": "Vermont","VA": "Virginia","WA": "Washington",
-      "WV": "West Virginia","WI": "Wisconsin","WY": "Wyoming"
-    }
     this.cols = [ "C150_4_2MOR", "C150_4_AIAN", "C150_4_ASIAN", "C150_4_BLACK", "C150_4_HISP", "C150_4_NHPI", "C150_4_NRA", "C150_4_UNKN", "C150_4_WHITE"];
   };
   
@@ -34,7 +22,7 @@ class Tooltip {
   tooltip_html(data, d) {
     var cols = this.cols;
     var perct = 0;
-    let text = "<h2>" + `${this.stateMapping[d]}` + "</h2>";
+    let text = "<h2>" + `${Tools.stateMapping[d]}` + "</h2>";
     cols.forEach(function(race) {
       var dta = data.filter(dt => {
         return dt.STABBR === d && dt[race] !== 'NULL'});
